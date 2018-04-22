05a11b5c7ea5ae8f2b3168fab66dd8077fa70393
AndrewLBeasley <alloydbeasley@gmail.com>
2018-04-22 14:33:22 -0500

    escape characters, special characters, variable(string) interpolation.

--- www/hello.php
+++ www/hello.php
@@ -6,7 +6,9 @@
 //cannot use hyphens to create variable names.
 //php is case sensitive. $message!=$Message.
 
-$message = 'Hello again!'; //type string, value Hello again.'
+//type string, value Hello again.'
+// can use \ to escape the string and add special characters such as \n to add a space between string characters.
+$message = 'Hello again!'; 
 
 //type number.
 //positive or negative numbers.
@@ -16,8 +18,6 @@ $count = 10;
 
 //decimal parts of numbers are known as floats. floats can also be negative.
 $price = 0.23;
-
-
 
 
 // echo tag tells web server that whatever follows should be printed to the browser.
@@ -31,7 +31,6 @@ var_dump($price);
 
 
 //php is a loosely typed language. you do not have to declare a variables type. php interprets the type itself.
-
 //variables can also contain boolean values: true or false.
 //useful for controlling program flow.
 // variables can also be null. variables that have no values, or have not been assigned values.
@@ -50,8 +49,13 @@ var_dump($count*$price);
 
 //php uses the . operator to concatenate string values. Unlike javascript which uses + to concatenate.
 //can use the . operator more than once. 
+//can also concatenate literal strings and variables. 
+//use "" to insert variables directly into a string. This is called variable interpolation.
 $name = 'Andrew';
 echo $message .''. $name;
+echo 'Hello ' . $name;
+echo "Hello $name"; //variable interpolation.
+echo "Hello {$name}";//use curly braces if you are not sure where the variable ends.
 
 //when performing operations on variables with different values, php will type juggle the result to try to determine the new type.
 //this operation will result in a new value of int, and reassign the value of $int_string to the type of int.
@@ -67,3 +71,5 @@ var_dump($int_string);
 $is_editor = true;
 $is_administrator = false;
 var_dump($is_editor);
+
+
