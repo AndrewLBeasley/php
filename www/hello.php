4afc1a0e82119cfcb341223ff9290df7745d9c06
AndrewLBeasley <alloydbeasley@gmail.com>
2018-04-22 13:48:29 -0500

    initial

---
+++ www/hello.php
@@ -0,0 +1,10 @@
+<?php // ?php opening tag that tells the web server that the code we are writing is in php.
+
+//using the $ declares a variable used to store information for the program to use. 
+//the message variable is assign hello again.
+//can use camelCase, full lowercase, numbers, and names including underscores eg Full_Name.
+//cannot use hyphens to create variable names.
+$message = 'Hello again!';
+
+// echo tag tells web server that whatever follows should be printed to the browser.
+echo $message;
