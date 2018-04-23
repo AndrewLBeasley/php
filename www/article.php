244eb31a6c77830f5993a73532e80517dc02053c
AndrewLBeasley <alloydbeasley@gmail.com>
2018-04-23 16:45:30 -0500

    func:new config file//config added to gitignore

---
+++ www/article.php
@@ -0,0 +1,84 @@
+<?php 
+
+// all of the information needed to connect to the local db
+$db_host = 'localhost';
+$db_name = 'cms';
+$db_user = 'cms_www';
+$db_pass = 'andrew22';
+
+// establishes connection to the DB using the mysqli_connect method.
+// connects to the db and returns a variable that represents a connection to the db server.
+//mysqli_connect parameters must be passed in IN THE ORDER BELOW
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+//use the mysqli_connect_error method to check your new connection.
+//returns a string if the connection fail, and returns null if the connection was successful.
+//an error will return a string with info about the error, therefor firing the conditional and printing the error message.
+if(mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit;
+    //calling exit will stop the script from firing.
+    //if the connection fails we dont want to continue executing the script. 
+}
+echo 'connected successfully';
+
+//creates variable with SQL query that pulls one article from the database that matches the query.
+//uses superglobal $_GET to find values with queried id
+//checks that passed in id is a numeric value to help avoid SQL injections. 
+//if the passed id is not numeric, no article will be retrieved.
+//use the isset function to determine whether a variable is set and not null. 
+//if the query string is set, and is numeric, the GET array will make a valid return
+
+if(isset($_GET['id']) && is_numeric($_GET['id'])){
+$sql = 'SELECT*
+        FROM article
+        WHERE id = ' . $_GET['id'];
+    ;
+
+
+
+//creates $results variable which will hold the results of the above query.
+//passes in connnection variable and sql statement.
+//will return false if there is an error retrieving the data. 
+$results = mysqli_query($conn, $sql);
+
+//if the query returns false, php will echo the error message.
+//must use strictly equals to perform the comparison to avoid edge cases of false being returned for values of null or empty arrays.
+if($results === false){
+    echo mysqli_error($conn);
+}else{
+    //uses mysqli_fetch_assoc to pull one row from the db table
+    $article = mysqli_fetch_assoc($results);
+    // var_dump($articles);
+}
+
+}else{
+    $articles = null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- checks to see if there is any content to post. if there is not, the message will display -->
+    <?php if($article === null):?>
+        <p> Article not found </p>
+    <?php else: ?>
+                    <!-- renders the article to html -->
+                        <article>
+                            <h2><?=$article['title'];?></h2>
+                            <p><?=$article['content'];?></p>
+                        </article>
+                   
+    <?php endif; ?>
+</body>
+
+</html>
---
+++ www/db_connection.php
@@ -0,0 +1,86 @@
+<?php 
+
+// all of the information needed to connect to the local db
+$database = include('config.php');
+
+$db_host = $database['host'];
+$db_name = $database['name'];
+$db_user = $database['user'];
+$db_pass = $database['pass'];
+
+// establishes connection to the DB using the mysqli_connect method.
+// connects to the db and returns a variable that represents a connection to the db server.
+//mysqli_connect parameters must be passed in IN THE ORDER BELOW
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+//use the mysqli_connect_error method to check your new connection.
+//returns a string if the connection fail, and returns null if the connection was successful.
+//an error will return a string with info about the error, therefor firing the conditional and printing the error message.
+if(mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit;
+    //calling exit will stop the script from firing.
+    //if the connection fails we dont want to continue executing the script. 
+}
+echo 'connected successfully';
+
+//creates variable with initial SQL query
+$sql = 'SELECT*
+        FROM article
+        ORDER BY published_at;'
+    ;
+
+//creates $results variable which will hold the results of the above query.
+//passes in connnection variable and sql statement.
+//will return false if there is an error retrieving the data. 
+$results = mysqli_query($conn, $sql);
+
+//if the query returns false, php will echo the error message.
+//must use strictly equals to perform the comparison to avoid edge cases of false being returned for values of null or empty arrays.
+if($results === false){
+    echo mysqli_error($conn);
+}else{
+    //sets the $articles variable to the response from the database on a successful query.
+    //passing MYSQLI_ASSOC into the fetch function will return the array as an associative array.
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    // var_dump($articles);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- checks to see if there is any content to post. if there is not, the message will display -->
+    <?php if(empty($articles)):?>
+        <p> No articles found </p>
+    <?php else: ?>
+            <!-- loops over returned information from the database and displays it in html -->
+            <ul>
+                <?php foreach($articles as $article): ?>
+                    <li>
+                        <article>
+                            <h2><a href='article.php?id=<?= $article['id'];?>'><?=$article['title'];?></a></h2>
+                            <h4><?=$article['content'];?></h4>
+                        </article>
+                    </li>
+                <?php endforeach; ?>
+            </ul>   
+    <?php endif; ?>
+</body>
+
+<!-- DB Quiz and recap -->
+<!-- data in a relational database is represented in tables -->
+<!-- use sql SELECT to select data from a database -->
+<!-- use WHERE to specify a condition when selecting data with an SQL query -->
+<!-- use ORDER BY to change the order in which information is returned from the database -->
+<!-- hostname, database name, username, and password are the four pieces of data required to connect to a database using PHP -->
+<!-- query results from a database are returned in an array -->
+<!-- PHP sends SQL to the database, which returns data, and HTML is used to format this data in the browser -->
+</html>
